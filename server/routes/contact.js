9165519d9a2981f16e7ef4ea3eb306dd8e319152
Riya Jain <jainriya6824@gmail.com>
2026-07-04 13:16:19 +0530

    Add email support using Nodemailer

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -4,7 +4,9 @@ import nodemailer from "nodemailer";
 const router = Router();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
